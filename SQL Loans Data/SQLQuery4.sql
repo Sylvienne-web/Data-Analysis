f202ce8f15ad7eb24a51d886832e39b481f9f0cc
Sylvienne-web <itspthao@gmail.com>
2024-12-31 15:10:55 +0700

    Update SQLQuery1.sql

--- SQL Loans Data/SQLQuery4.sql
+++ SQL Loans Data/SQLQuery4.sql
@@ -1,4 +1,10 @@
--- Question 1
+/* 1. Report the fin_id, dpd_type, year and month of date of each loan in the tracking_payment_dpd table.
+ dpd_type is defined as:
+ 1. If a fin_id has dpd>30: dpd_type1
+ 2. If a fin_id has dpd<=30 in current month, and it had dpd>0 in immediately preceding (previous) month: dpd_type2
+ 3. If a fin_id has dpd<=30 in current month, but no dpd in immediately preceding (previous) month: dpd_type3
+ Return the result table ordered in ascending order by fin_id,year and month.
+*/
 WITH temp_table AS (
 	SELECT
 		*,
@@ -37,7 +43,7 @@ GROUP BY
 	DATEPART(month, [date])
 ORDER BY fin_id, _year, _month
 
--- Question 2
+-- 2. Report the fin_id, average not due amount in cents & maximum total amount due in cents for dates in Oct'2022 from tracking_performance table. For fin_ids with no dates in Oct’22, return null values for both average not due amount and maximum of not due amount.
 WITH t1 AS (
 	SELECT 
 		fin_id,
@@ -58,6 +64,3 @@ RIGHT JOIN (
 	FROM tracking_performance
 ) t2
 ON t1.fin_id = t2.fin_id
-
-
-
