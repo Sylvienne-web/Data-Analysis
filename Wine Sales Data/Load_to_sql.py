feb9c211901ffbd8555a3ec7e06054142684d33c
Sylvienne-web <itspthao@gmail.com>
2024-12-31 13:55:10 +0700

    Update Load_to_sql.py

--- Wine Sales Data/Load_to_sql.py
+++ Wine Sales Data/Load_to_sql.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 26 12:10:33 2024
-
-@author: thao
-"""
 import pandas as pd
 import pyodbc, urllib, math
 from sqlalchemy import create_engine
 from sqlalchemy.types import NVARCHAR
 
-file = r"C:\Users\phanh\Downloads\Technical_Test_BI\data_sales.csv"
-
+# 1. Open csv file into defined dictionary
+file = r"your_path"
 data_types = {'Date': 'object',
  'Store Number': 'object',
  'Store Name': 'object',
@@ -18,28 +12,28 @@ data_types = {'Date': 'object',
  'Category Name': 'object',
  'Vendor Number': 'object',
  'Vendor Name': 'object'}
-
 data = pd.read_csv(file, dtype=data_types)
 
+# 2. Format datetime to SQL Server's standards
 data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y')
 data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
 
-# t1 = data[0:100]
-
-# Columns' names in SQL Server cannot have space
+# 3. Drop space in column names
 data.columns = data.columns.str.replace(' ', '_')
 
-csv_file_dest = r"C:\Users\phanh\Downloads\Technical_Test_BI\data_sales_standardized.csv"
+# 4. Save raw data after manipulating
+csv_file_dest = r"C:\your_dest_path"
 data.to_csv(csv_file_dest)
 
-sqlserver = urllib.parse.quote_plus(r"Driver={ODBC Driver 17 for SQL Server};Server=PTHAOKEKE\SQLEXPRESS;Database=interview_data;Trusted_Connection=Yes;")
+# 5. Set up SQL Server & specify location to load data
+sqlserver = urllib.parse.quote_plus(r"Driver={ODBC Driver 17 for SQL Server};Server=your_server;Database=your_database;Trusted_Connection=Yes;")
 engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(sqlserver),fast_executemany=True)
-
 tablename='HMD_TABLE'
 schemaname = 'dbo'
 txt_cols = data.select_dtypes(include = ['object']).columns
 columns = {col_name: NVARCHAR for col_name in txt_cols}
 
+# 6. Load every 500,000 rows to SQL Server
 start = 0
 end = math.ceil(len(data)/100000)*100000
 step = 500000
